96f9e11feeacca0e15002b0a70d82cb68821c703
sreekanth <sreekanthj009@gmail.com>
2019-08-07 11:50:57 +0530

    movie app done

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,94 @@
 import fetch from 'isomorphic-unfetch';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+import styles from '../styles/app.scss';
+// import CardMedia from '@material-ui/core/CardMedia';
+
+const imageBaseUrl = 'https://image.tmdb.org/t/p/w500';
+
+const posRelative = {
+  position: 'relative',
+}
 
 const Index = props => (
   console.log('data', props.data),
-  console.log('dataList', props.dataList),
+  console.log('dataList', props.dataList.results),
 
-  <Container fixed>
-    <Grid container spacing={3}>
-      <Grid item xs={6}>
-        <h1>Movie Name</h1>
+  <Container fixed className={'contaier'}>
+
+    <Grid container spacing={3} className={'movie-thumbnail-block'}>
+      <Grid item xs={12} md={2}>
+        <div className={'movie-thumbnail'}>
+          <img src={`${imageBaseUrl}${props.data.backdrop_path}`} alt={props.data.title} />
+        </div>
       </Grid>
-      <Grid item xs={6}>
-        <h1>Movie Details</h1>
+      <Grid item xs={12} md={7} style={posRelative}>
+        <div className={'movie-details'}>
+          <div className={'movie-title'}>
+            <h3>{props.data.title}</h3>
+            <button>star</button>
+          </div>
+          <div className={'button-group'}>
+            <Button variant="contained" className={'play'}>
+              Play
+            </Button>
+            <Button variant="contained" className={'watch-later'}>
+              Watch Later
+            </Button>
+            <Button variant="contained" color="primary" className={'share'}>
+              Share
+            </Button>
+          </div>
+        </div>
       </Grid>
     </Grid>
+
+    <Grid container className={'movie-details-block'}>
+      <Grid item xs={12} md={6} className={'movie-synopsis'}>
+        <h2>Synopsis</h2>
+        <p>
+          {props.data.overview}
+        </p>
+      </Grid>
+      <Grid item xs={12} md={6} className={'movie-banner'}>
+        <img src={`${imageBaseUrl}${props.data.backdrop_path}`} alt={props.data.title} />
+      </Grid>
+    </Grid>
+
+    <div className={'related-movie-block'}>
+      <h3>Related Videos</h3>
+      <Grid container className={'related-movie-grid'}>
+        {
+          props.dataList.results.map((item) => {
+            return (
+              <Grid item xs={12} sm={6} md={3} className={'related-movie-list'}>
+                <Card className={'related-movie-card'}>
+                  <CardMedia
+                    component={'img'}
+                    alt={item.title}
+                    image={`${imageBaseUrl}${item.backdrop_path}`}
+                  />
+                  <CardContent>
+                    <Typography gutterBottom>
+                      {item.title}
+                    </Typography>
+                  </CardContent>
+                  <Button variant="contained" color="primary" className={'play'}>
+                    Play
+                  </Button>
+                </Card>
+              </Grid>
+            );
+          })
+        }
+      </Grid>
+    </div>
+
   </Container>
 );
 
